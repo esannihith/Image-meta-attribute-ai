915dcb6d2ebb186e600ffbbbd122cef9205eb3f9
esannihith <esanni04@gmail.com>
2025-05-18 11:50:08 +0530

    cleanfirstcommit

--- install-test-deps.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-echo Installing test dependencies for frontend...
-cd frontend
-npm install --save-dev vitest @testing-library/react @testing-library/react-hooks @testing-library/dom @testing-library/user-event @babel/eslint-parser eslint
-echo Finished installing test dependencies.
-pause
